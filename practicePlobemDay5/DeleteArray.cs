9c86ab19124dddd406efbe4452438e4a792a11f5
arivusurya <arivusurya.3@gmail.com>
2023-05-11 22:00:28 +0530

    question1

---
+++ practicePlobemDay5/DeleteArray.cs
@@ -0,0 +1,18 @@
+namespace practicePlobemDay5
+{
+    public class DeleteArray
+    {
+        public void Deletemethodint(int[] array,int element){
+            array = Array.FindAll(array , item => item != element);
+            System.Console.WriteLine(String.Join(',',array));
+        }
+         public void Deletemethodfloat(float[] array,float element){
+            array = Array.FindAll(array , item => item != element);
+            System.Console.WriteLine(String.Join(',',array));
+        }
+          public void Deletemethodchar(char[] array,char element){
+            array = Array.FindAll(array , item => item != element);
+            System.Console.WriteLine(String.Join(',',array));
+        }
+    }
+}
---
+++ practicePlobemDay5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace practicePlobemDay5{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            DeleteArray deleteArray = new DeleteArray();
+            int[] intarray = {1,2,3,5,5,7,9};
+            float[] floatarray = {3.4f,1.2f,1.8f};
+            char[] chararray = "Lucifer".ToArray();
+            deleteArray.Deletemethodint(intarray,5);
+            deleteArray.Deletemethodfloat(floatarray,1.8f);
+            deleteArray.Deletemethodchar(chararray,'f');
+        }
+    }
+}
